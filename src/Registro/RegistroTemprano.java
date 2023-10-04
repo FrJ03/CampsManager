5c4fcb02e94eaeb71796779be8e2d6f7c41368e7
i12telol <118206793+i12telol@users.noreply.github.com>
2023-10-04 15:38:43 +0200

    Creado RegistroTemprano.java

---
+++ src/Registro/RegistroTemprano.java
@@ -0,0 +1,32 @@
+package Registro
+
+public class RegistroTemprano extends AbstractRegistro{
+  /**
+  *Constructor sin parámetros
+  **/
+  public RegistroTemprano(){
+    super();
+  }
+
+
+  /**
+  * Registra la inscripción
+  **/
+  @Override
+  public registro(){
+      if(this.fechaInscripción /**si la fecha de inscripcion se lleva 15 dias minimo con la fecha de inicio del campamento**/){
+        
+      }
+    else{
+      print
+  }
+  /**
+  * Cancela la inscripción
+  **/
+  @Override
+  public cancelacion(){
+    
+  }
+
+  
+}
