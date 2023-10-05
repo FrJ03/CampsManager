6fec869f307791a3b186db71dacad6973752ece0
i12telol <118206793+i12telol@users.noreply.github.com>
2023-10-05 18:40:17 +0200

    Update AbstractRegistro.java

--- src/Registro/AbstractRegistro.java
+++ src/Registro/AbstractRegistro.java
@@ -1,19 +1,37 @@
 package Registro;
+import java.util.Date;
 
-  /**
-	* Clase que representa datos báscicos de registros que se pueden hacer en una inscripcion
+  	/**
+	* Clase que representa datos básicos de registros que se pueden hacer en una inscripcion
  	@author Lucía Téllez López
 	**/
 
 public abstract class AbstractRegistro(){
-  /**
-  *Constructor sin parámetros
-  **/ 
-  public Registro() {}
-  
+
+	/**
+ 	* Atributos del registro, "reserva" expresa si la reserva se ha registrado correctamente
+  	y "idAsociado" muestra el ID del participante del campamento que se asocia a la reserva
+  	**/
+	private boolean reserva;
+	private int idAsociado;
+	
+	/**
+ 	* Constructor sin parámetros
+ 	**/
+	public abstract Registro(){}
+	/**
+ 	* Constructor con parámetros, en la función registro
+  	se cambiará el valor de la reserva a true si se
+   	cumplen los requisitos
+ 	**/
+	public abstract Registro(boolean reserva, int idAsociado){
+		this.reserva=false;
+		this.idAsociado=idAsociado;
+	}
+	
   /**
   *Funciones que permiten registrar o cancelar un registro
   **/
-  public abstract registro();
+  public abstract registro(Date fechaInscripcion, Date inicioCampamento);
   public abstract cancelacion();
 }
