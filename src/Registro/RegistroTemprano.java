02de22d11ffd9c33a093919aee48c9a86b50b15c
i12telol <118206793+i12telol@users.noreply.github.com>
2023-10-05 18:59:42 +0200

    Update RegistroTemprano.java

--- src/Registro/RegistroTemprano.java
+++ src/Registro/RegistroTemprano.java
@@ -1,4 +1,5 @@
 package Registro
+import java.uil.Date;
 
 public class RegistroTemprano extends AbstractRegistro{
   /**
@@ -7,26 +8,44 @@ public class RegistroTemprano extends AbstractRegistro{
   public RegistroTemprano(){
     super();
   }
+  /**
+  *Constructor con parámetros
+  **/
+  public RegistroTemprano(boolean reserva, int idAsociado){
+    super(reserva,idAsociado);
+  }
 
 
   /**
   * Registra la inscripción
+  Recibe las fechas de inscripción e inicio del campamento, las transforma para poder
+  restarlas y comparar los días de diferencia.
+  Si hay 15 días o más entre la fecha de inscripción y la fecha de comienzo del
+  campamento, se puede realizar el registro.
   **/
   @Override
-  public registro(){
-      if(this.fechaInscripción /**si la fecha de inscripcion se lleva 15 dias minimo con la fecha de inicio del campamento**/){
-        
+  public void registro(Date fechaInscripcion, Date inicioCampamento){
+      long diaInscripcion = fechaInscripcion.getTime() ; 
+      long diaCampamento = fechaHasta.getTime();
+      long diaInicioInsc = (long) Math.floor(diaInscripcion / (1000*60*60*24));
+      long diaInicioCamp = (long) Math.floor(diaCampamento / (1000*60*60*24));
+      long diferencia = diaInicioCamp - diaInicioInsc;
+    
+    if(diferencia<15){
+        System.out.println("Se ha pedido un registro a un campamento que empieza en menos de 15 días, no se ha permitido.");
       }
     else{
-      print
+        this.reserva=true;
+        System.out.println("Se ha realizado el registro correctamente.");
+    }
   }
   /**
   * Cancela la inscripción
   **/
   @Override
-  public cancelacion(){
-    
+  public void cancelacion(){
+    this.reserva=false;
+    System.out.println("Se ha cancelado la reserva");
   }
-
-  
+    
 }
