1140d5ce927d092c611d5150f5298775ff608488
Yo <franjomor@gmail.com>
2023-10-05 12:38:22 +0200

    Comentarios de la clase Asistente añadidos.

--- src/Asistente/Asistente.java
+++ src/Asistente/Asistente.java
@@ -15,7 +15,7 @@ import Persona.Persona;
      /**
       * Fecha de nacimiento del asistente
       * */
-     private Date fecha_nacimiento;
+     private Date fechaNacimiento;
 
      /**
       * Construye un objeto vacio*/
@@ -23,30 +23,58 @@ import Persona.Persona;
 
      }
      /**
-      * Construye un objeto con todos sus datos
+      * Construye un objeto con todos sus datos.
+      * @param id Identificador único del asistente.
+      * @param nombre Nombre del asistente.
+      * @param apellidos Apellidos del asistente.
+      * @param fechaNacimiento Representa la fecha de nacimiento del asistente.
+      * @param especial Indica si en asistente pertenece a un grupo especial (true) o no (false).
       * */
-     public Asistente(int id, String nombre, String apellidos, Date fecha_nacimiento,boolean especial){
+     public Asistente(int id, String nombre, String apellidos, Date fechaNacimiento,boolean especial){
     	 super(id, nombre, apellidos);
-        this.fecha_nacimiento = fecha_nacimiento;
+        this.fechaNacimiento = fechaNacimiento;
         this.especial = especial;
      }
-     public Date get_fecha_nacimiento() {
-    	 return this.fecha_nacimiento;
+     /**
+      * Devuelve la fecha de nacimiento del asistente.
+      * */
+     public Date get_fechaNacimiento() {
+    	 return this.fechaNacimiento;
      }
-     public boolean get_especial() {
+     /**
+      * Devuelve si asistente necesita atención especial.
+      * @return true si es necesaria una atención especial, false en caso contrario.
+      * */
+     public boolean getEspecial() {
     	 return especial;
      }
-     public void set_fecha_nacimiento(Date fecha_nacimiento) {
-    	 this.fecha_nacimiento = fecha_nacimiento;
+     /**
+      * Establece una nueva fecha de nacimiento del asistente.
+      * @param fechaNacimiento fecha de nacimiento a establecer.
+      */
+     public void setFechaNacimiento(Date fechaNacimiento) {
+    	 this.fechaNacimiento = fechaNacimiento;
      }
-     public void set_especial(boolean especial) {
+     /**
+      * Modifica si el asistente necesita atención especial.
+      * @param especial true si es requerida una atención especial, false en caso contrario.
+      */
+     public void setEspecial(boolean especial) {
     	 this.especial = especial;
      }
+     /**
+      * Función que devuelve una cadena de caracteres con información del asistente.
+      * @return La estrucutra es la siguiente:
+      * Id: (id)\n
+      * Nombre Completo: (nombreCompleto)\n
+      * Fecha de Nacimiento: (fechaNacimiento)\n
+      * Grupo Especial: (Si/No)\n
+      */
      public String toString() {
     	 String ret ="Id: " + this.get_id()+ 
     			 	"\nNombre Completo: " + this.get_nombre_completo()+ 
-    			 	"\nFecha de nacimiento: " + this.fecha_nacimiento.toString() + 
-    			 	"\nGrupo especial: ";
+    			 	"\nFecha de Nacimiento: " + this.fechaNacimiento.toString() + 
+    			 	"\nGrupo Especial: ";
     	 if(this.especial) {
     		 ret.concat("Si\n");
     	 }
