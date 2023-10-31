3f88f02941ea5b0048ad5984e5a4c648430695c6
Manuel Garcia Obrero <43857650+TnTManu@users.noreply.github.com>
2023-10-31 12:45:24 +0100

    Añadir constructor

--- src/data/CampamentoActividadDTO.java
+++ src/data/CampamentoActividadDTO.java
@@ -3,29 +3,39 @@ package data;
 /**
  * DTO utilizado para hacer uso de los métodos del DAO CampamentoActividad.
  * @author Enrique de los Reyes Montilla
+ * @author Manuel García Obrero
+ * 
  */
 public class CampamentoActividadDTO {
 	/**
 	 * Variable privada que representa el id de un campamento.
 	 */
-	private int camId_;
+	private int campId_;
 	/**
 	 * Variable privada que representa el id de una actividad.
 	 */
 	private int actId_;
 	/**
+	  * Costruye un objeto con todos sus datos.
+	  * @return void
+	  * */
+	public CampamentoActividadDTO(int campId, int actId) {
+		this.campId_ = campId;
+		this.actId_=actId;
+	};
+	/**
 	 * Método que devuelve el id de un campamento.
 	 * @return int
 	 */
-	public int getCamId() {
-		return camId_;
+	public int getCampId() {
+		return campId_;
 	}
 	/**
 	 * Método que asigna el id de un campamento a la variable privada.
-	 * @param camId Id del campamento que se va a asignar.
+	 * @param campId Id del campamento que se va a asignar.
 	 */
-	public void setCamId(int camId) {
-		this.camId_ = camId;
+	public void setCampId(int camId) {
+		this.campId_ = camId;
 	}
 	/**
 	 * Método que devuelve el id de una actividad.
