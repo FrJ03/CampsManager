649355d4922207405eb04879cb47d1deec66c718
EnriReyes <reyesmontillae@gmail.com>
2023-11-30 21:17:43 +0100

    Creacion de templates ver campamento y volver al index.

--- src/main/java/controller/servlets/SearchCampament.java
+++ src/main/java/controller/servlets/SearchCampament.java
@@ -1,6 +1,8 @@
 package controller.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import controller.dto.activity.Nivel;
+import controller.dto.camp.CampDTO;
+import controller.dto.monitor.MonitorDTO;
 import view.beans.customer.CustomerBean;
 
 
@@ -43,7 +48,7 @@ public class SearchCampament extends HttpServlet {
 	            dispatcher.forward(request, response);
 	        } else {
 	        	// El cliente existe y tiene el rol de cliente
-	        	RequestDispatcher dispatcher = request.getRequestDispatcher("/mvc/view/assistant/searchCampament.jsp");
+	        	RequestDispatcher dispatcher = request.getRequestDispatcher("/mvc/view/assistant/searchCampamentForm.jsp");
 	        	request.setAttribute("message", "");
 	            dispatcher.forward(request, response);
 	        }
@@ -66,12 +71,54 @@ public class SearchCampament extends HttpServlet {
 	        String educationalLevel = request.getParameter("educationalLevel");
 	        String availableSeatsStr = request.getParameter("availableSeats");
 	    if ((educationalLevel == null && availableSeatsStr == "") ) {
-	    	RequestDispatcher dispatcher = request.getRequestDispatcher("/mvc/view/assistant/searchCampament.jsp");
+	    	RequestDispatcher dispatcher = request.getRequestDispatcher("/mvc/view/assistant/searchCampamentForm.jsp");
         	request.setAttribute("message", "The value of the request is invalid");
             dispatcher.forward(request, response);
 	    }
 	    
 	    if(educationalLevel != null) {
+	        RequestDispatcher dispatcher = request.getRequestDispatcher("/include/templates/campTemplate.jsp");
+            dispatcher.include(request, response);
+	    	CampDTO camp = new CampDTO();
+	    	camp.setId(1);
+	        camp.setInicioCampamento(LocalDate.of(2023, 1, 1));
+	        camp.setFinCampamento(LocalDate.of(2023, 1, 10));
+	        camp.setNivel(Nivel.Adolescente);
+	        camp.setAsistentesMax(50);
+
+	        // Create and set a MonitorDTO for the responsible
+	        MonitorDTO responsible = new MonitorDTO();
+	        responsible.setId(101);
+	        responsible.setNombre("John Doe");
+	        camp.setResponsable(responsible);
+
+	        // Create and set a MonitorDTO for the special responsible
+	        MonitorDTO specialResponsible = new MonitorDTO();
+	        specialResponsible.setId(102);
+	        specialResponsible.setNombre("Jane Doe");
+	        camp.setResponsableEspecial(specialResponsible);
+	        response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+	        out.println("<ul>\n");
+	        out.println("<li><strong>Id:</strong> "+camp.getId()+"</li>\n");
+	        out.println("<li><strong>Start Date:</strong> " + camp.getInicioCampamento() +"</li>\n");
+	        out.println("<li><strong>End Date:</strong> "+camp.getFinCampamento() + "</li>\n");
+	        out.println("<li><strong>Academic Level:</strong> " + camp.getNivel() + "</li>\n");
+	        out.println("<li><strong>Maximum Number of Attendees:</strong>  " +camp.getAsistentesMax() + "</li>\n");
+	        out.println("<li><strong>Responsible Monitor:</strong> "+camp.getResponsable()+"</li>\n");
+
+	        // Monitor Especial information
+	        out.println("<li><strong>Monitor Especial:</strong> ");
+	        if (camp.getResponsableEspecial() != null) {
+	            out.println(camp.getResponsableEspecial());
+	        } else {
+	            out.println("Empty");
+	        }
+	        out.println("</li>\n");
+
+	        out.println("</ul>");
+	        dispatcher = request.getRequestDispatcher("/include/templates/returnToIndex.jsp");
+            dispatcher.include(request, response);
 	    }
        
 	}
