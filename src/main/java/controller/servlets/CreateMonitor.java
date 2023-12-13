8eb9974f0a97befb60d6fa05567bc98bbdbea64b
Yo <franjomor@gmail.com>
2023-12-13 14:08:36 +0100

    Errors solved in CreateMonitor

--- src/main/java/controller/servlets/CreateMonitor.java
+++ src/main/java/controller/servlets/CreateMonitor.java
@@ -1,9 +1,9 @@
 package controller.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import controller.dto.monitor.*;
 
 import controller.gestores.GestorCampamentos;
 import view.beans.customer.CustomerBean;
@@ -20,17 +20,22 @@ public class CreateMonitor extends HttpServlet{
                       			disp.forward(request, response);
                 		}
                 		else {
-                      			String name = request.getParameter("name");
-                      			String last = request.getParameter("last_names");
-                      			bool special = request.getParameter("special");
+                  			String name = request.getParameter("name");
+                  			String last = request.getParameter("last_names");
+                  			String special = request.getParameter("special");
 
-                            MonitorDTO mon=new MonitorDTO(name,last,Boolean.parseBoolean(special));
-                			
-                      			if(!isBoolean(special) || !GestorCampamentos.getInstance().crearMonitor(mon)) {
-                            				RequestDispatcher disp = request.getRequestDispatcher("/include/errors/errorAddMonitor.html");
-                            				disp.forward(request, response);
-                      			}
-                		}
+                            MonitorDTO mon=new MonitorDTO(-1, name,last,Boolean.parseBoolean(special));
+                				
+                            if(!special.equalsIgnoreCase("true") && !special.equalsIgnoreCase("false")) {
+                            	RequestDispatcher disp = request.getRequestDispatcher("/include/errors/errorAddMonitor.html");
+            					disp.forward(request, response);
+                			}
+                            
+                  			if(!isBoolean(special) || !GestorCampamentos.getInstance().crearMonitor(mon)) {
+                        				RequestDispatcher disp = request.getRequestDispatcher("/include/errors/errorAddMonitor.html");
+                        				disp.forward(request, response);
+                  			}
+	            		}
           	}
             private boolean isBoolean(String value) {
                     return value != null && (value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false"));
