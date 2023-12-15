70801309db577b7d270f94627127e0beca0e4469
Yo <franjomor@gmail.com>
2023-12-15 19:09:07 +0100

    AssistantDTO javadoc modified

--- src/main/java/controller/dto/assistant/AssistantDTO.java
+++ src/main/java/controller/dto/assistant/AssistantDTO.java
@@ -5,39 +5,41 @@ import java.io.Serializable;
 import controller.dto.person.Person;
 
 /**
- * Clase que representa a un asistente al campamento
+ * Class that representa an assistant
  * @author Francisco José Mellado Ortiz
  */
  public class AssistantDTO extends Person implements Serializable, Comparable<AssistantDTO>{
 	 /**
-	  * Representa si el asistente necesita atención especial
-	  * */
+	  * Especial assistant requirements. True if it is needed, false otherwise
+	  */
      private boolean especial_;
      /**
-	  * Representa el email del asistente
-	  * */
+	  * Email address
+	  */
      private String email_;
      /**
-      * Fecha de nacimiento del asistente
-      * */
+      * Birth date
+      */
      private LocalDate fechaNacimiento_;
 
      /**
-      * Construye un objeto vacio*/
+      * Empty constructor
+      */
      public AssistantDTO(){
 
      }
-     /**Método que indica la versión de la clase que se va a serializar.
- 	 */
+     /**
+      * Serial version attribute
+      */
  	 private static final long serialVersionUID = 3L;
      /**
-      * Construye un objeto con todos sus datos.
-      * @param id Identificador único del asistente.
-      * @param nombre Nombre del asistente.
-      * @param apellidos Apellidos del asistente.
-      * @param fechaNacimiento Representa la fecha de nacimiento del asistente.
-      * @param especial Indica si en asistente pertenece a un grupo especial (true) o no (false).
-      * @param email Email del asistente
+      * Constructor.
+      * @param id Assistant identifier
+      * @param nombre Assistant name.
+      * @param apellidos Assistant lastname.
+      * @param fechaNacimiento Assistant birth date.
+      * @param especial Especial requirements. True if it's needed, false otherwise
+      * @param email Email address
       * */
      public AssistantDTO(int id, String email, String nombre, String apellidos, LocalDate fechaNacimiento,boolean especial){
     	 super(id, nombre, apellidos);
@@ -46,52 +48,54 @@ import controller.dto.person.Person;
         this.email_ = email;
      }
      /**
-      * Devuelve la fecha de nacimiento del asistente.
-      * */
+      * Birth date get method
+      * @return LocalDate Birth date
+      */
      public LocalDate get_fechaNacimiento() {
     	 return this.fechaNacimiento_;
      }
      /**
-      * Devuelve si asistente necesita atención especial.
-      * @return true si es necesaria una atención especial, false en caso contrario.
-      * */
+      * Especial requirements get method
+      * @return boolean true if the assistant require special attention, false otherwise
+      */
      public boolean getEspecial() {
     	 return especial_;
      }
      /**
-      * Devuelve el email del asistente.
-      * @return string Email del asistente.
-      * */
+      * Email get method
+      * @return String Assistant email
+      */
      public String getEmail() {
     	 return email_;
      }
      /**
-      * Establece una nueva fecha de nacimiento del asistente.
-      * @param fechaNacimiento fecha de nacimiento a establecer.
+      *	Birth date set method
+      * @param fechaNacimiento New birth date
       */
      public void setFechaNacimiento(LocalDate fechaNacimiento) {
     	 this.fechaNacimiento_ = fechaNacimiento;
      }
      /**
-      * Modifica si el asistente necesita atención especial.
-      * @param especial true si es requerida una atención especial, false en caso contrario.
+      * Special requirements set method
+      * @param boolean true if the assistant require special attention, false otherwise
       */
      public void setEspecial(boolean especial) {
     	 this.especial_ = especial;
      }
      /**
-      * Modifica el email del asitente.
-      * @param String Nuevo email del asistente.
+      * Email set method
+      * @param String New email
       */
      public void setEmail(String email) {
     	 this.email_ = email;
      }
      /**
-      * Función que devuelve una cadena de caracteres con información del asistente.
-      * @return La estrucutra es la siguiente:
+      * Method that returns a String that contains assistant data
+      * @return String The structure is:
       * Id: (id)\n
       * Nombre Completo: (nombreCompleto)\n
       * Fecha de Nacimiento: (fechaNacimiento)\n
+      * Email del assistente: (email)\n
       * Grupo Especial: (Si/No)\n
       */
      public String toString() {
@@ -108,6 +112,10 @@ import controller.dto.person.Person;
     	 }
     	 return ret;
      }
+     /**
+      * Method that compare assistants
+      * @return int 0 if both assistants has the same id, -1 if this assistant has an id less than the other or 1 otherwise
+      */
      @Override
      public int compareTo(AssistantDTO a) {
 		if(a.getId() > this.getId())
