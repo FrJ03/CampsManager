a91b77ca38c2a6b56ba31e400041e1988a623c1c
Yo <franjomor@gmail.com>
2023-12-15 17:00:22 +0100

    AdminDTO modified

--- src/main/java/controller/dto/admin/AdminDTO.java
+++ src/main/java/controller/dto/admin/AdminDTO.java
@@ -1,27 +1,16 @@
 package controller.dto.admin;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-
 import controller.dto.person.Person;
 
 /**
  * Class that represents an admin
  */
 public class AdminDTO extends Person implements Serializable,  Comparable<AdminDTO>{
-	/**
-	  * Representa si el admin necesita atención especial
-	  */
-    private boolean especial_;
     /**
 	  * Representa el email del admin
 	  * */
     private String email_;
-    /**
-     * Fecha de nacimiento del admin
-     * */
-    private LocalDate fechaNacimiento_;
-
     /**
      * Construye un objeto vacio*/
     public AdminDTO(){
@@ -36,27 +25,10 @@ public class AdminDTO extends Person implements Serializable,  Comparable<AdminD
      * @param email Email del admin
      * @param nombre Nombre del admin.
      * @param apellidos Apellidos del admin.
-     * @param fechaNacimiento Representa la fecha de nacimiento del admin.
-     * @param especial Indica si en admin pertenece a un grupo especial (true) o no (false).
      * */
-    public AdminDTO(int id, String email, String nombre, String apellidos, LocalDate fechaNacimiento,boolean especial){
+    public AdminDTO(int id, String email, String nombre, String apellidos){
    	 super(id, nombre, apellidos);
-       this.fechaNacimiento_ = fechaNacimiento;
-       this.especial_ = especial;
        this.email_ = email;
-    }
-    /**
-     * Devuelve la fecha de nacimiento del admin.
-     * */
-    public LocalDate get_fechaNacimiento() {
-   	 return this.fechaNacimiento_;
-    }
-    /**
-     * Devuelve si admin necesita atención especial.
-     * @return true si es necesaria una atención especial, false en caso contrario.
-     * */
-    public boolean getEspecial() {
-   	 return especial_;
     }
     /**
      * Devuelve el email del admin.
@@ -64,20 +36,6 @@ public class AdminDTO extends Person implements Serializable,  Comparable<AdminD
      * */
     public String getEmail() {
    	 return email_;
-    }
-    /**
-     * Establece una nueva fecha de nacimiento del admin.
-     * @param fechaNacimiento fecha de nacimiento a establecer.
-     */
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-   	 this.fechaNacimiento_ = fechaNacimiento;
-    }
-    /**
-     * Modifica si el admin necesita atención especial.
-     * @param especial true si es requerida una atención especial, false en caso contrario.
-     */
-    public void setEspecial(boolean especial) {
-   	 this.especial_ = especial;
     }
     /**
      * Modifica el email del asitente.
@@ -97,15 +55,7 @@ public class AdminDTO extends Person implements Serializable,  Comparable<AdminD
     public String toString() {
    	 String ret ="Id: " + this.getId()+ 
    			 	"\nNombre Completo: " + this.getNombreCompleto()+ 
-   			 	"\nFecha de Nacimiento: " + this.fechaNacimiento_.toString() +
-   			 	"\nEmail del admin: " + this.email_ +
-   			 	"\nGrupo Especial: ";
-   	 if(this.especial_) {
-   		 ret += "Si\n";
-   	 }
-   	 else {
-   		 ret += "No\n";
-   	 }
+   			 	"\nEmail del admin: " + this.email_;
    	 return ret;
     }
     @Override
