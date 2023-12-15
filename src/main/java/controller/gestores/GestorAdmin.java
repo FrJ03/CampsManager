d163da3a3e8dad7fda1ceb0a3899f4ddbc4bdb52
Yo <franjomor@gmail.com>
2023-12-15 20:11:56 +0100

    GestorAdmin javadoc modified

--- src/main/java/controller/gestores/GestorAdmin.java
+++ src/main/java/controller/gestores/GestorAdmin.java
@@ -3,22 +3,22 @@ package controller.gestores;
 import controller.dto.admin.AdminDTO;
 import model.dao.AdminDAO;
 /**
- * Clase que gestiona la información de los admin al campamento
+ * Class that manage administrators implemented using Singleton design pattern
  * @author Enrique de los Reyes Montilla
  */
 public class GestorAdmin {
 	/**
-	 * Variable privada Singleton.
+	 * Singleton private instance
 	 */
 	private static GestorAdmin instance_ = null;
 
 	/**
-	 * Constructor privado que crea una lista de Admin vacia.
+	 * Singleton private constructor
 	 */
 	private GestorAdmin() {};
 	/**
-	 * Metodo que sirve de acceso a la instancia.
-	 * @return Instancia de la clase GestorAdmins.
+	 * Instance access method
+	 * @return GestorAdmin Instance
 	 */
 	public static GestorAdmin getInstance() {
 		if(instance_ == null ) {
@@ -27,31 +27,29 @@ public class GestorAdmin {
 		return instance_;
 	}
 	/**
-	 * Metodo que añade a la lista de Admin un nuevo Admin pasado como argumento, comprobando si no estaba registrado. Si estaba registrado devuelve false y si no lo estaba true
-	 * @param AdminDTO Admin que se va a añadir a la lista de Admin.
-	 * @return boolean(false error, true no error).
+	 * Method that add an admin
+	 * @param AdminDTO Admin to add.
+	 * @return boolean True if the assistant has been added correctly, false otherwise
 	 */
 	public boolean darAltaAdmin(AdminDTO Admin) {	
 		AdminDAO db = AdminDAO.getInstance();
 		return db.create(Admin);
 	}
 	/**
-	 * Metodo que devuelve un admin de la base de datos
-	 * @param email Email del admin que va a ser leido.
-	 * @return AdminDTO
+	 * Method that read an admin
+	 * @param email Email to search for the admin
+	 * @return AdminDTO Admin if the admin exists, null otherwise
 	 */
 	public AdminDTO leerAdmin(String email) {	
 		AdminDAO db = AdminDAO.getInstance();
 		return db.read(email);
 	}
 	/**
-	 * Metodo para dar de alta un admin.
-	 * @param email Email del admin.
-  	 * @param nombre Nombre del admin.
-  	 * @param apellidos Apellidos del admin.
-  	 * @param fechaNacimiento Representa la fecha de nacimiento del admin.
-  	 * @param especial Indica si en admin pertenece a un grupo especial (true) o no (false).
-	 * @return void.
+	 * Method that add an admin
+	 * @param email Admin email
+	 * @param nombre Admin name
+	 * @param apellidos Admin lastname
+	 * @return boolean True if the assistant has been added correctly, false otherwise
 	 */
 	public boolean darAltaAdmin(String email, String nombre, String apellidos){
 		AdminDAO db = AdminDAO.getInstance();
@@ -64,16 +62,14 @@ public class GestorAdmin {
 		return db.create(aux);
 	}
 	/**
-	 * Metodo Modificar toda la información de un Admin identificado por su id.
-	 * @param id Identificador único del Admin que se desea modificar.
-	 * @param email Email del Admin.
-  	 * @param nombre Nombre del Admin.
-  	 * @param apellidos Apellidos del Admin.
-  	 * @param fechaNacimiento Representa la fecha de nacimiento del Admin.
-  	 * @param especial Indica si en Admin pertenece a un grupo especial (true) o no (false).
-	 * @return void.
+	 * Method that modified an admin
+	 * @param id Admin id to modify
+	 * @param email New email
+  	 * @param nombre New name
+  	 * @param apellidos New lastname
+	 * @return boolean true if the admin has been modified, false otherwise
 	 */
-	public boolean modificarAdmin(int id, String email, String nombre, String apellidos, String fechaNacimiento, String especial){
+	public boolean modificarAdmin(int id, String email, String nombre, String apellidos){
 		AdminDAO db = AdminDAO.getInstance();
         
 		return db.update(new AdminDTO(id, email, nombre, apellidos));
