22aa894eb5dc8a8013d62aafea567b0285a0595b
Manuel Garcia Obrero <43857650+TnTManu@users.noreply.github.com>
2023-12-21 04:35:48 +0100

    añadir todo

--- src/main/java/controller/servlets/DoRegistration.java
+++ src/main/java/controller/servlets/DoRegistration.java
@@ -1,1 +1,46 @@
+package controller.servlets;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import controller.gestores.GestorInscripciones;
+import view.beans.customer.CustomerBean;
+
+/**
+ * Servlet implementation class to create a new activity
+ * @author Manuel Garcia Obrero
+ */
+public class DoRegistration extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		CustomerBean customer = (CustomerBean) session.getAttribute("customerBean");
+		if(customer == null || !customer.getRol().equals("Admin")) {
+			RequestDispatcher disp = request.getRequestDispatcher("/include/errors/errorRol.jsp");
+			disp.forward(request, response);
+		}
+		else {
+			String idUser = request.getParameter("idUser");
+			String idCamp = request.getParameter("idCamp");
+			String type = request.getParameter("type");
+		
+			
+			if(type=="Parcial") {
+				if(!GestorInscripciones.getInstance().realizarRegistroParcial(Integer.parseInt(idCamp), Integer.parseInt(idUser))) {
+					RequestDispatcher disp = request.getRequestDispatcher("/include/errors/errordoRegistration.html");
+					disp.forward(request, response);
+				}
+			}
+			if(type=="Completa") {
+				if(!GestorInscripciones.getInstance().realizarRegistroCompleto(Integer.parseInt(idCamp), Integer.parseInt(idUser))) {
+					RequestDispatcher disp = request.getRequestDispatcher("/include/errors/errordoRegistration.html");
+					disp.forward(request, response);
+				}
+			}
+		}
+	}
+}
 
