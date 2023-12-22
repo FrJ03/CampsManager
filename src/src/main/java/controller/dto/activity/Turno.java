4b992899f67c164c60af99da52b05447aff424a8
Yo <franjomor@gmail.com>
2023-12-22 19:13:27 +0100

    Errors solved

--- src/src/main/java/controller/dto/activity/Turno.java
+++
@@ -1,9 +0,0 @@
-package controller.dto.activity;
-/**
- * Enum class that represents the schedule of an activity
- * @author Enrique de los Reyes Montilla
- */
-public enum Turno {
-	Morning,
-	Afternoon
-}
